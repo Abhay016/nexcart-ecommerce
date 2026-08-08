1b53efa85dac82206b7ee23342978c7d0b207147
Abhay016 <jaiswal.abhay161@gmail.com>
2026-08-08 21:40:36 +0530

    WebConfig added

---
+++ nexcart-backend/src/main/java/com/nexcart/security/WebConfig.java
@@ -0,0 +1,21 @@
+package com.nexcart.security;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Value("${nexcart.frontend.url}")
+    private String frontendUrl;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(frontendUrl)
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+    
+}
